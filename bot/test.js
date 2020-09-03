26713be3bd6bfe3466cfe941192f080e77d116e7
YunYouJun <me@yunyoujun.cn>
2020-09-03 17:16:24 +0800

    :sparkles: add jsdoc custom plugin example

--- bot/test.js
+++ bot/test.js
@@ -1,3 +1,13 @@
-module.exports = () => {
-  console.log("cool");
+const { default: Bot } = require("el-bot");
+/**
+ *
+ * @param {Bot} ctx
+ */
+module.exports = (ctx) => {
+  const mirai = ctx.mirai;
+  mirai.on("message", (msg) => {
+    msg.reply(
+      "我是一个无情的自定义测试插件，可以到 el/index.yml plugins.custom 处关闭我。"
+    );
+  });
 };
